5f53ef1535f7e20e85f5c0d4cac53b98dbc55566
kelno <kelno@users.noreply.github.com>
2017-03-29 12:46:46 +0200

    Update AsyncWorker.php

--- AsyncWorker.php
+++ AsyncWorker.php
@@ -2,10 +2,6 @@
 
 /* Object pcntl_fork wrapper
 Usages example are included as comments below
-
-/!\ Important note: If given command return evaluates to false, this is considered an error. 
-This is because call_user_func_array return false on error, so I can't distinguish between 
-the function returning false or an error. Possible fix: http://stackoverflow.com/questions/11767728/what-if-call-user-func-is-supposed-to-return-false
 */
 class ASyncWorker {
 
@@ -20,38 +16,47 @@ class ASyncWorker {
         $this->success_file_handler = tmpfile();
         if(!$this->result_file_handler || !$this->success_file_handler)
         {
+            trigger_error("ASyncWorker: Failed to create result or success file handles", E_USER_ERROR);
             $this->failed = true;
             return;
         }
         $this->pid = pcntl_fork();
 
         if ($this->pid === -1) {
+            trigger_error("ASyncWorker: Failed to fork", E_USER_ERROR);
             $this->failed = true;
         } elseif ($this->pid === 0) {
             // $pid = 0, this is the child thread
 
             $args = func_get_args();
-            if(sizeof($args) < 1)
+            if(sizeof($args) < 1) {
+                trigger_error("ASyncWorker: No function to execute given", E_USER_ERROR);
                 exit(1);
+            }
 
             $function = $args[0];
             array_shift($args); //remove function from array, leaving only function arguments
 
-            $result = call_user_func_array($function, $args);
-            if($result == FALSE) {
+            if(!is_callable($function)) {
+                trigger_error("ASyncWorker: Function $function not found or not callable", E_USER_ERROR);
                 exit(2);
             }
 
+            $result = call_user_func_array($function, $args);
+            /* do not check result value, call_user_func_array will return false on error, but error may be a valid return value of the given function.
+            I assume is_callable prevent all errors cases for call_user_func_array, this may be wrong
+            */
+
             $ok = fwrite($this->result_file_handler, serialize($result));
             if(!$ok) {
-                echo "Failed to write" . PHP_EOL;
-                exit(4);
+                trigger_error("ASyncWorker: Failed to write result file", E_USER_ERROR);
+                exit(3);
             }
 
             $ok = fwrite($this->success_file_handler, self::SUCCESS_CODE);
             if(!$ok) {
-                echo "Failed to write success " . PHP_EOL;
-                exit(6);
+                trigger_error("ASyncWorker: Failed to write success file", E_USER_ERROR);
+                exit(4);
             }
 
             exit(0); //exit this fork
@@ -66,12 +71,8 @@ class ASyncWorker {
         pcntl_waitpid($this->pid, $status);
 
         //anormal exit !
-        if(!pcntl_wifexited($status)) {
-            $this->failed = true;
-            return false;
-        }
-
-        if(pcntl_wexitstatus($status) != 0) {
+        if(!pcntl_wifexited($status) || pcntl_wexitstatus($status) != 0) {
+            trigger_error("ASyncWorker: Forked process did not exit properly", E_USER_ERROR);
             $this->failed = true;
             return false;
         }
@@ -79,7 +80,7 @@ class ASyncWorker {
         fseek($this->success_file_handler, 0);
         $success = fgets($this->success_file_handler);
         if($success != self::SUCCESS_CODE) {
-            echo "Success file not found or invalid" . PHP_EOL;
+            trigger_error("ASyncWorker: Success file not found or invalid", E_USER_ERROR);
             $this->failed = true;
             return false;
         }
@@ -87,7 +88,7 @@ class ASyncWorker {
         fseek($this->result_file_handler, 0);
         $result_serialized = fgets($this->result_file_handler);
         if($result_serialized === false) {
-            echo "No result in file or couln't read it" . PHP_EOL;
+            trigger_error("ASyncWorker: No data in result file, or couldn't read it", E_USER_ERROR);
             $this->failed = true;
             return false;
         }
@@ -106,9 +107,13 @@ class ASyncWorker {
 }
 
 /* Examples
+
 function my_funky_function($arg1, $arg2)
 {
-    sleep(1);
+    // do some work here
+    sleep(1); //simulate working time
+
+    // test arguments
     echo $arg1 . PHP_EOL;
     echo $arg2 . PHP_EOL;
 
@@ -117,7 +122,10 @@ function my_funky_function($arg1, $arg2)
 
 function my_funky_function_array($arg1, $arg2, $arg3)
 {
-    sleep(2);
+    // do some work here
+    sleep(2); //simulate working time
+
+    // test arguments
     echo $arg1 . PHP_EOL;
     echo $arg2 . PHP_EOL;
     echo $arg3 . PHP_EOL;
